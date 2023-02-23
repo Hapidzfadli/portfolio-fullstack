51ae3758bbccc76fc45d2ee7f5dfb8ffa74c220c
hapidzfadli <hapidzfadli@gmail.com>
2023-02-24 02:04:43 +0700

    edit appjs dan client

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -2,11 +2,11 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-  projectId: "xz1cj6qr",
+  projectId: "jujwg837",
   dataset: 'production',
   apiVersion: '2023-02-11',
   useCdn: true,
-  token: "sk7lQ6WrN5oG6ETD76UKa3xXebGCpDh25tmKQrKOCkWz9xg8O0HwvHSpr6MMF3BiPYsQpleX2Kk7sqkx6V8kJ3rp3GDnOalS5cI06biHxth1H20PHWJvmORgQFH0dOOOZ46jcPDxs9OmLFKA4ADf8t5ngLkTwkEz1Cy5iDZUbyiPmdGPI14V",
+  token: "skvz2Ys5zoBXZkxk74CbDh66wOA6d9t6tgwgePnhR7ABz5WE5eFIVMSTVnxV6pP34mwe3sKO5qDvVExmlQ8CU5lvnl2WRmvuM49Chy7oqkN7FxIp52qZpl1TZog1pYYnf5SBaDcch70Q80PKuMHVhHRvpljFXWqhpGgFp6AFUy8jZoBL8b1a",
 });
 
 const builder = imageUrlBuilder(client);
